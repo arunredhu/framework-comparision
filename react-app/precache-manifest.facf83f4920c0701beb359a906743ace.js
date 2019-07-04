2a0d80a5e39762c1abd2b2a12f5840626c936266
Arun Redhu <arun.redhu05@gmail.com>
2019-07-04 15:08:56 +0530

    React build updated

---
+++ react-app/precache-manifest.facf83f4920c0701beb359a906743ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0a009389c33fd3c5d54535101d4fc9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f99cac25924ce8753bb3",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e1fb5ee1ae12933f98de",
+    "url": "./static/js/2.ff55df81.chunk.js"
+  },
+  {
+    "revision": "f99cac25924ce8753bb3",
+    "url": "./static/js/main.0aed8c21.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
