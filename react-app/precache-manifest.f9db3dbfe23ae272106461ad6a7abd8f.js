39eb520d8be0a6cb9fc010fc38dd2cb3f28db299
Arun Redhu <arun.redhu05@gmail.com>
2019-07-04 13:45:21 +0530

    initial commit

---
+++ react-app/precache-manifest.f9db3dbfe23ae272106461ad6a7abd8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29190cc04dd64801ac07de6a52de70a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5ab1511cb20eb660b71",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e1fb5ee1ae12933f98de",
+    "url": "/static/js/2.ff55df81.chunk.js"
+  },
+  {
+    "revision": "b5ab1511cb20eb660b71",
+    "url": "/static/js/main.49ae2c0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
